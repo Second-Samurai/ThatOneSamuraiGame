753666bb48b03517ccc5dbc8b6e5c292d26ca898
Kyle Hammer <kylethomashammer@gmail.com>
2020-08-17 20:27:37 +1000

    [UI05, CR05, DT01, DT04, VE07] Project Setup

---
+++ Assets/ScriptableObjectInterfaces/GameObjectRuntimeSet.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GameObject RuntimeSet")]
+public class GameObjectRuntimeSet : RuntimeSet<GameObject>
+{
+    
+}
---
+++ Assets/ScriptableObjectInterfaces/RuntimeSet.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class RuntimeSet<T> : ScriptableObject
+{
+    private List<T> items = new List<T>();
+
+    public void Add(T t)
+    {
+        if(!items.Contains(t)) items.Add(t);
+    }
+    
+    public void Remove(T t)
+    {
+        if(items.Contains(t)) items.Remove(t);
+    }
+
+    public T GetItemIndex(int index)
+    {
+        return items[index];
+    }
+    
+    public void PrintItemList()
+    {
+        if (items.Count > 0)
+        {
+            for(int i = items.Count - 1; i >= 0; i--)
+            {
+                Debug.Log("Item " + i + ": " + items[i]);
+            }
+        }
+        else
+        {
+            Debug.Log("No items in this list");
+        }
+    }
+
+    public void Clear()
+    {
+        items.Clear();
+    }
+
+    public int Count()
+    {
+        return items.Count;
+    }
+}

caac144883a05645d0d488ecc375c2b954e9d5ab
Ayielin <ailin.gist@gmail.com>
2020-08-31 00:28:59 +1000

    merge conflict resolution

---
+++ Assets/Scripts/Enemies/ArcherTempTracker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcherTempTracker : MonoBehaviour
+{
+    EnemyTracker _enemyTracker;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && _enemyTracker == null)
+        {
+            _enemyTracker = GameManager.instance.enemyTracker;
+            _enemyTracker.AddEnemy(this.transform);
+             
+        }
+    }
+}

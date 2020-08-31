a18a81610a66b0339cfaacb217007b9b55bd9cd4
Jaiden Chicote <jedezzio@gmail.com>
2020-08-31 13:02:34 +1000

    Basic dev setup for gravity grounding detection

---
+++ Assets/Scripts/Player/PFeetGrounded.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PFeetGrounded : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void RunGroundCheck()
+    {
+
+    }
+
+    //private Vector3 RaycastToGround()
+    //{
+
+    //}
+}

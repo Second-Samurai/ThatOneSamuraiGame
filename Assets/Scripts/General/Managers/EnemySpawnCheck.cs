0da74556b5094c0f0a5f8b451f4246a189fd6368
Ayielin <ailin.gist@gmail.com>
2020-09-28 19:51:34 +1000

    merge conflicts for additive loading

--- Assets/Scripts/General/Managers/EnemySpawnCheck.cs
+++ Assets/Scripts/General/Managers/EnemySpawnCheck.cs
@@ -26,9 +26,9 @@ public class EnemySpawnCheck : MonoBehaviour
     public void CheckIfSpawned()
     {
         bool bToBeSpawned = true;
-        spawnManager.enemySpawnDictionary.TryGetValue(myName, out bToBeSpawned);
-        Debug.LogError(myName + " " + bToBeSpawned);
-        gameObject.SetActive(bToBeSpawned);
+        if (spawnManager.enemySpawnDictionary.TryGetValue(myName, out bToBeSpawned))
+            gameObject.SetActive(bToBeSpawned);
+        else gameObject.SetActive(true);
     }
 
     //private void OnDisable()

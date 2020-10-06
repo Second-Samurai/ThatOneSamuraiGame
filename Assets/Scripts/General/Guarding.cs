b63344b8ca1549102f4002836343bbea959f0571
KyleHammer <kylethomashammer@gmail.com>
2020-10-06 22:15:26 +1100

    -

--- Assets/Scripts/General/Guarding.cs
+++ Assets/Scripts/General/Guarding.cs
@@ -56,9 +56,6 @@ public class Guarding : MonoBehaviour
         
         if (canGuard && !isStunned)
         {
-            // Stop all animation triggers from running
-            _aiSystem.ResetAnimationVariables();
-            
             StopAllCoroutines();
             CalculateGuard(damage);
             return true;

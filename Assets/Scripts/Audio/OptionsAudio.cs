a077d8360a8d524c10b6c58edddf0ccddfcea3af
Laith <monkeylaith@gmail.com>
2020-10-18 23:41:56 +1100

    no message

---
+++ Assets/Scripts/Audio/OptionsAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsAudio : MonoBehaviour
+{
+    public AudioClip optionsSelect;
+    public AudioClip startGame;
+
+    public AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GameManager.instance.audioManager.backgroundAudio.optionsSelectSource;
+        startGame = GameManager.instance.audioManager.FindSound("selectbuttonsfx");
+        optionsSelect = GameManager.instance.audioManager.FindSound("scrollingsfx");
+    }
+
+    public void PlaySelect()
+    {
+        audioSource.clip = startGame;
+        audioSource.PlayOneShot(startGame, 1);
+    }
+
+    public void ButtonSelect()
+    {
+        audioSource.clip = optionsSelect;
+        audioSource.PlayOneShot(optionsSelect, 1);
+    }
+}

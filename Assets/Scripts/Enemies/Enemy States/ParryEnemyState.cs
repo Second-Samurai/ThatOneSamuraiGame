8433274eee81472fc5600f389b7f6ac78e2d3859
KyleHammer <kylethomashammer@gmail.com>
2020-10-20 14:56:18 +1100

    [AI34] Minor tweaks before speeding up parry attacks

--- Assets/Scripts/Enemies/Enemy States/ParryEnemyState.cs
+++ Assets/Scripts/Enemies/Enemy States/ParryEnemyState.cs
@@ -15,15 +15,15 @@ namespace Enemies.Enemy_States
         public override IEnumerator BeginState()
         {
             AISystem.attackIndicator.ShowIndicator();
+            
             // Disable canParry
             AISystem.eDamageController.enemyGuard.canParry = false;
             
-            // Make the next attack unblockable
-            //AISystem.bIsUnblockable = true;
+            // Stop the enemy block sword effect and play the parry effect
             AISystem.swordEffects.EndBlockEffect();
-            //AISystem.swordEffects.BeginUnblockableEffect();
-
             AISystem.parryEffects.PlayParry();
+            
+            // Make a decision to determine the next move
             int decision = Random.Range(0, 4);
 
             if (AISystem.enemyType == EnemyType.TUTORIALENEMY) //TUTORIAL ENEMIES CANNOT USE UNBLOCKABLE
@@ -34,7 +34,7 @@ namespace Enemies.Enemy_States
                 // Set the attack trigger
                 Animator.SetTrigger("TriggerLightAttack");
             }
-            else if (decision == 2) // Thrust
+            else if (decision == 2) // Heavy attack
             {
                 if (AISystem.enemyType == EnemyType.GLAIVEWIELDER)
                 {
@@ -48,36 +48,35 @@ namespace Enemies.Enemy_States
                 }
             }
             
-            else if (decision == 3)
+            else if (decision == 3) // Counter attack
             {
                 Animator.SetTrigger("TriggerCounterAttack");
 
             }
-            // Set the parry trigger
-            
+
             yield break;
             
-            // NOTE: End state is called through an animation event in the parry attack animation
+            // NOTE: End state is called through an animation event at the end of the attack animation
         }
         
         public override void Tick()
         {
             // Get target position and face towards it
-            _target = AISystem.enemySettings.GetTarget().position + AISystem.floatOffset;
-            PositionTowardsTarget(AISystem.transform, _target);
+            // _target = AISystem.enemySettings.GetTarget().position + AISystem.floatOffset;
+            // PositionTowardsTarget(AISystem.transform, _target);
         }
 
         // End state is called by animation event
         public override void EndState()
         {
             AISystem.attackIndicator.HideIndicator();
+            
             // Ensure rotate to player is set back in end state
             bIsRotating = true;
-            AISystem.EndUnblockable();
-            // Restore future attacks to be blockable
-            AISystem.bIsUnblockable = false;
-           // AISystem.swordEffects.EndUnblockableEffect();
             
+            // End the unblockable sword and effect
+            AISystem.EndUnblockable();
+
             ChooseActionUsingDistance(AISystem.enemySettings.GetTarget().position + AISystem.floatOffset);
         }
     }

e36baec4a570118b43caad5efacdfd3639e8064c
Ayielin <ailin.gist@gmail.com>
2020-11-11 19:06:44 +1100

    particles spawned on death

---
+++ Assets/Scripts/Enemies/EnemyDeathParticleSpawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeathParticleSpawn : MonoBehaviour
+{
+    ParticleSystem particles;
+    [HideInInspector] public bool bIsPlaying = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+    }
+
+    public void SpawnParticles()
+    {
+       if(!particles.isPlaying) particles.Play();
+    }
+
+   
+}

72f705e5692a8891d25c51d89fd6356b5c0d04c6
DESKTOP-8I2U4EN\Jaiden Chicote <jcsfproductionsutube@gmail.com>
2020-11-14 01:25:32 +1100

    Teleportation ring levitation functionality

--- Assets/Scripts/Levels/LevelSpecificScripts/LTeleportationRings.cs
+++ Assets/Scripts/Levels/LevelSpecificScripts/LTeleportationRings.cs
@@ -1,22 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem.XR.Haptics;
+
 
 public class LTeleportationRings : MonoBehaviour
 {
+    public enum RingState
+    {
+        Idle,
+        OnEnter,
+        OnLeave
+    };
+
     [Header("Rings")]
     public Transform outerRing;
     public Transform innerRing;
     public Transform teleportationPoint;
 
-    [HideInInspector]
+    [Header("Ring Starting Values")]
+    public Vector3 startingPosition;
+    public float startingXRotation;
+
+    [Header("Ring End Values")]
+    public float innerEndYPosition;
+    public float innerEndXRotation;
+    public float outerEndYPosition;
+    public float outerEndXRotation;
+
+    public RingState ringState;
     public bool canBeUsed = false;
     public bool isActive = false;
-    public bool isPlayerDetected = false;
+    private bool hasAnimated = false;
 
     private LTeleportationRings destinationRing;
     private UITeleportation teleportationUI;
+
+    private float outerYPosition;
+    private float innerYPosition;
+
+    public bool canInnerRotate = true;
+    public bool canOuterRotate = true;
+
+    private float refY;
+    private float refYY;
 
     // Start is called before the first frame update
     void Start()
@@ -28,20 +54,121 @@ public class LTeleportationRings : MonoBehaviour
     private void FixedUpdate()
     {
         OnActive();
+        AnimateRingLevitation();
     }
 
     private void OnActive()
     {
-        transform.Rotate(0, 15, 0);
+        if (isActive)
+        {
+            transform.Rotate(0, 15, 0);
+        }
     }
 
     private void AnimateRingLevitation()
     {
+        if (ringState == RingState.OnEnter)
+        {
+            AnimateOn();
+        } 
+        else if(ringState == RingState.OnLeave)
+        {
+            AnimateClose();
+        }
+    }
 
+    private void AnimateOn()
+    {
+        outerYPosition = 0;
+        innerYPosition = 0;
+
+        if (canOuterRotate)
+        {
+            outerRing.transform.Rotate(0.4f, 0, 0);
+            if (outerRing.transform.eulerAngles.x >= (0 + outerEndXRotation))
+            {
+                outerRing.transform.rotation = Quaternion.Euler(new Vector3(0 + outerEndXRotation, outerRing.eulerAngles.y, outerRing.eulerAngles.z));
+                canOuterRotate = false;
+            }
+        }
+
+        outerYPosition = Mathf.SmoothDamp(outerRing.transform.localPosition.y, outerEndYPosition, ref refY, 0.7f);
+        outerRing.localPosition = new Vector3(outerRing.localPosition.x, outerYPosition, outerRing.localPosition.z);
+
+        if (canInnerRotate)
+        {
+            innerRing.transform.Rotate(-0.4f, 0, 0);
+            if (innerRing.transform.eulerAngles.x <= (360 + innerEndXRotation))
+            {
+                innerRing.transform.rotation = Quaternion.Euler(new Vector3(360 + innerEndXRotation, innerRing.eulerAngles.y, innerRing.eulerAngles.z));
+                canInnerRotate = false;
+            }
+        }
+
+        innerYPosition = Mathf.SmoothDamp(innerRing.transform.localPosition.y, innerEndYPosition, ref refYY, 0.7f);
+        innerRing.localPosition = new Vector3(innerRing.localPosition.x, innerYPosition, innerRing.localPosition.z);
+
+        //Debug.Log(outerYPosition);
+        //Debug.Log(Mathf.Ceil(innerYPosition));
+
+        if (!canOuterRotate && !canInnerRotate && outerYPosition >= (outerEndYPosition-0.5f) && innerYPosition >= (innerEndYPosition - 0.5f))
+        {
+            Debug.Log("Is still running");
+            ringState = RingState.Idle;
+        }
+    }
+
+    private void AnimateClose()
+    {
+        outerYPosition = 0;
+        innerYPosition = 0;
+
+        if (canOuterRotate)
+        {
+            outerRing.transform.Rotate(-0.4f, 0, 0);
+            if (outerRing.transform.eulerAngles.x <= 0)
+            {
+                outerRing.transform.rotation = Quaternion.Euler(new Vector3(0, outerRing.eulerAngles.y, outerRing.eulerAngles.z));
+                canOuterRotate = false;
+            }
+        }
+
+        outerYPosition = Mathf.SmoothDamp(outerRing.transform.localPosition.y, -1, ref refY, 0.7f);
+        outerRing.localPosition = new Vector3(outerRing.localPosition.x, outerYPosition, outerRing.localPosition.z);
+
+        if (canInnerRotate)
+        {
+            innerRing.transform.Rotate(0.4f, 0, 0);
+            if (innerRing.transform.eulerAngles.x >= 360)
+            {
+                innerRing.transform.rotation = Quaternion.Euler(new Vector3(0, innerRing.eulerAngles.y, innerRing.eulerAngles.z));
+                canInnerRotate = false;
+            }
+        }
+
+        innerYPosition = Mathf.SmoothDamp(innerRing.transform.localPosition.y, -1, ref refYY, 0.7f);
+        innerRing.localPosition = new Vector3(innerRing.localPosition.x, innerYPosition, innerRing.localPosition.z);
+
+        if (Mathf.Floor(outerYPosition) == -1 && Mathf.Floor(innerYPosition) == -1)
+        {
+            ringState = RingState.Idle;
+            canInnerRotate = false;
+            canOuterRotate = false;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (!canBeUsed) return;
+
+        //isPlayerDetected = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!canBeUsed) return;
+
+        //isPlayerDetected = false;
     }
 }
+

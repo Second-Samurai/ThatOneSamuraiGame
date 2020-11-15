4e123d8449534abc19a91c83be733773d92ce3b7
KyleHammer <kylethomashammer@gmail.com>
2020-11-15 20:45:13 +1100

    Layermasks Work!!!

--- Assets/Scripts/Camera/LockOnCamera/AddToLockOnTracker.cs
+++ Assets/Scripts/Camera/LockOnCamera/AddToLockOnTracker.cs
@@ -8,20 +8,24 @@ public class AddToLockOnTracker : MonoBehaviour
     private LockOnTracker _lockOnTracker;
     private LineRenderer _lineRenderer;
 
-    private LayerMask _layerMask;
+    public LayerMask layerMask;
+    public float _raycastMaxDist;
 
     private Transform _cameraTransform;
 
     //Offset since the target point is at the enemy's feet
     private Vector3 _offset = Vector3.up * 2;
+    
+    //Debug controls to show the line renderer
+    [SerializeField] private bool bLineRendererDebug = false;
 
     private void Start()
     {
+        _raycastMaxDist = GetComponent<SphereCollider>().radius * 2;
+
         _lockOnTracker = GameManager.instance.lockOnTracker;
         
         _cameraTransform = Camera.main.transform;
-
-        _layerMask = GetLayerMask();
 
         _lineRenderer = GetComponent<LineRenderer>();
         _lineRenderer.widthMultiplier = 0.2f;
@@ -41,21 +45,35 @@ public class AddToLockOnTracker : MonoBehaviour
         {
             Vector3 rayStartPos = _cameraTransform.position;
 
-            if (Physics.Raycast(rayStartPos, (enemyTransform.position - rayStartPos).normalized, out hit, _layerMask))
+            if (Physics.Raycast(rayStartPos, (enemyTransform.position - rayStartPos).normalized, out hit, _raycastMaxDist, layerMask))
             {
-                _lineRenderer.enabled = true;
                 if (hit.collider.CompareTag("Enemy"))
                 {
-                    Debug.Log("Success: " + hit.collider.gameObject.name);
-                    _lineRenderer.startColor = Color.white;
+                    
                 }
                 else
                 {
-                    Debug.Log(hit.collider.gameObject.name);
-                    _lineRenderer.startColor = Color.black;
+                    
                 }
-                _lineRenderer.SetPosition(0, rayStartPos);
-                _lineRenderer.SetPosition(1, enemyTransform.position + _offset);
+                
+                #region Debug for Line Rendering
+                if (bLineRendererDebug)
+                {
+                    _lineRenderer.enabled = true;
+                    if (hit.collider.CompareTag("Enemy"))
+                    {
+                        Debug.Log("Success: " + hit.collider.gameObject.name);
+                        _lineRenderer.startColor = Color.green;
+                    }
+                    else
+                    {
+                        Debug.Log(hit.collider.gameObject.name);
+                        _lineRenderer.startColor = Color.red;
+                    }
+                    _lineRenderer.SetPosition(0, rayStartPos);
+                    _lineRenderer.SetPosition(1, enemyTransform.position + _offset);
+                }
+                #endregion
             }
         }
     }
@@ -73,19 +91,5 @@ public class AddToLockOnTracker : MonoBehaviour
         {
             _lockOnTracker.RemoveEnemy(other.transform);
         }
-    }
-
-    private LayerMask GetLayerMask()
-    {
-        int defaultLayer = 1 << LayerMask.NameToLayer("Default");
-        int navMeshPathableLayer = 1 << LayerMask.NameToLayer("NavMeshPathable");
-        int structuresLayer = 1 << LayerMask.NameToLayer("Structures");
-        int enemyLayer = 1 << LayerMask.NameToLayer("Enemy");
-
-        LayerMask layerMask = defaultLayer | navMeshPathableLayer | structuresLayer | enemyLayer;
-        
-        Debug.Log("Layermask: " + layerMask.value);
-
-        return layerMask;
     }
 }

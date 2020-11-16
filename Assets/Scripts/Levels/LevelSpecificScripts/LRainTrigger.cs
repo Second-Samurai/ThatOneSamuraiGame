06a5ce8ecadc08a69855b5756f2a3120e58a1da8
DESKTOP-8I2U4EN\Jaiden Chicote <jcsfproductionsutube@gmail.com>
2020-11-16 22:46:51 +1100

    Rain Trigger

---
+++ Assets/Scripts/Levels/LevelSpecificScripts/LRainTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LRainTrigger : MonoBehaviour
+{
+    public GameObject rainParticles;
+
+    private void Awake()
+    {
+        rainParticles.SetActive(false);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        rainParticles.SetActive(true);
+    }
+}

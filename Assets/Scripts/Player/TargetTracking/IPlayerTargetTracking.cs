5d5dbe02d919fab77aab7e99d0262b4c384b96b5
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-06-27 02:07:02 +1000

    Add PlayerTargetTracking component

---
+++ Assets/Scripts/Player/TargetTracking/IPlayerTargetTracking.cs
@@ -0,0 +1,19 @@
+﻿namespace ThatOneSamuraiGame.Scripts.Player.TargetTracking
+{
+    
+    public interface IPlayerTargetTracking
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        void ToggleLockLeft();
+        
+        void ToggleLockOn();
+
+        void ToggleLockRight();
+
+        #endregion Methods
+
+    }
+    
+}
---
+++ Assets/Scripts/Player/TargetTracking/PlayerTargetLocking.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace ThatOneSamuraiGame.Scripts.Player.TargetTracking
+{
+    
+    public class PlayerTargetLocking : MonoBehaviour, IPlayerTargetTracking
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        public void ToggleLockLeft()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ToggleLockOn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ToggleLockRight()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion Methods
+        
+    }
+    
+}

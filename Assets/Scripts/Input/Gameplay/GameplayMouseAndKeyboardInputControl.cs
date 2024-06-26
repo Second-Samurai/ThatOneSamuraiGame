410ea952b45f0a3d159e816cedf80dbbaa40a9a9
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-06-26 21:55:34 +1000

    Create class for InputManager and GameplayInputControl

---
+++ Assets/Scripts/Input/Gameplay/GameplayMouseAndKeyboardInputControl.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace ThatOneSamuraiGame.Scripts.Input.Gameplay
+{
+    
+    public class GameplayMouseAndKeyboardInputControl : MonoBehaviour
+    {
+        
+        #region - - - - - - Fields - - - - - -
+
+        
+        
+        #endregion Fields
+        
+        #region - - - - - - Properties - - - - - -
+
+        
+        
+        #endregion Properties
+        
+        #region - - - - - - Lifecycle Methods - - - - - -
+
+        private void Start()
+        {
+            
+        }
+        
+        #endregion Lifecycle Methods
+        
+        #region - - - - - - Event Handlers - - - - - -
+
+        private void OnMovement(InputValue inputValue)
+        {
+            
+        }
+
+        private void OnSprint(InputValue inputValue)
+        {
+            
+        }
+
+        private void OnLockOn(InputValue inputValue)
+        {
+            
+        }
+        
+        #endregion Event Handlers
+        
+    }
+    
+}
---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace ThatOneSamuraiGame.Scripts.Input
+{
+    
+    /// <summary>
+    /// Manages the input possession between scenes.
+    /// </summary>
+    public class InputManager : MonoBehaviour
+    {
+        
+        #region - - - - - - - Fields - - - - - - - 
+        #endregion Fields
+
+        #region - - - - - - -  Properties - - - - - - - 
+        #endregion Properties
+
+        #region - - - - - - - Lifecycle Methods - - - - - - -
+
+        private void Start()
+        {
+
+        }
+
+        #endregion
+
+        #region - - - - - - -  Event Handlers - - - - - - - 
+        #endregion
+        
+    }
+    
+}

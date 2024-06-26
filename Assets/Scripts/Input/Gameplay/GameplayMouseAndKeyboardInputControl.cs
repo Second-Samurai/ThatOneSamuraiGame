98054d70ebd13831d248b8feb533291203e1947c
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-06-27 02:08:53 +1000

    Add input handlers to GameplayMouseAndKeyboardInputControl
    - Hook up added components to this class

--- Assets/Scripts/Input/Gameplay/GameplayMouseAndKeyboardInputControl.cs
+++ Assets/Scripts/Input/Gameplay/GameplayMouseAndKeyboardInputControl.cs
@@ -1,52 +1,146 @@
-﻿using UnityEngine;
+﻿using System;
+using ThatOneSamuraiGame.Scripts.Player.Movement;
+using ThatOneSamuraiGame.Scripts.Player.TargetTracking;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace ThatOneSamuraiGame.Scripts.Input.Gameplay
 {
     
+    /// <summary>
+    /// Handles inputs for Gameplay actions for Mouse and Keyboard.
+    /// </summary>
     public class GameplayMouseAndKeyboardInputControl : MonoBehaviour
     {
         
         #region - - - - - - Fields - - - - - -
 
-        
+        private IPlayerMovement m_PlayerMovement;
+        private IPlayerTargetTracking m_PlayerTargetTracking;
+
+        private bool m_IsInputActive = false;
+        private bool m_IsInputPaused = false;
         
         #endregion Fields
-        
-        #region - - - - - - Properties - - - - - -
-
-        
-        
-        #endregion Properties
         
         #region - - - - - - Lifecycle Methods - - - - - -
 
         private void Start()
         {
-            
+            this.m_PlayerMovement = GameManager.instance.GameState.ActivePlayer
+                                        .GetComponent<IPlayerMovement>();
+            this.m_PlayerTargetTracking = GameManager.instance.GameState.ActivePlayer
+                                            .GetComponent<IPlayerTargetTracking>();
+
+            this.m_IsInputActive = true;
         }
         
         #endregion Lifecycle Methods
         
         #region - - - - - - Event Handlers - - - - - -
 
+        // -----------------------------------------------------
+        // Movement related Events
+        // -----------------------------------------------------
+        
         private void OnMovement(InputValue inputValue)
         {
-            
+            if (!this.m_IsInputActive || this.m_IsInputPaused) return;
+            this.m_PlayerMovement.MovePlayer(inputValue.Get<Vector2>());
         }
 
         private void OnSprint(InputValue inputValue)
         {
-            
+            if (!this.m_IsInputActive || this.m_IsInputPaused) return;
+            this.m_PlayerMovement.SprintPlayer(inputValue.isPressed);
         }
+        
+        // -----------------------------------------------------
+        // Target locking related Events
+        // -----------------------------------------------------
 
         private void OnLockOn(InputValue inputValue)
         {
-            
+            if (!this.m_IsInputActive || this.m_IsInputPaused) return;
+            this.m_PlayerTargetTracking.ToggleLockOn();
+        }
+
+        private void OnToggleLockLeft(InputValue inputValue)
+        {
+            if (!this.m_IsInputActive || this.m_IsInputPaused) return;
+            this.m_PlayerTargetTracking.ToggleLockLeft();
+        }
+
+        private void OnToggleLockRight(InputValue inputValue)
+        {
+            if (!this.m_IsInputActive || this.m_IsInputPaused) return;
+            this.m_PlayerTargetTracking.ToggleLockRight();
+        }
+        
+        // -----------------------------------------------------
+        // Weapon / Attack related Events
+        // -----------------------------------------------------
+        
+        private void OnAttack(InputValue inputValue) 
+        {
+            if (!this.m_IsInputActive || this.m_IsInputPaused) return;
+        }
+
+        private void OnSwordDraw(InputValue inputValue)
+        {
+            if (!this.m_IsInputActive || this.m_IsInputPaused) return;
+        }
+
+        private void OnStartHeavy(InputValue inputValue)
+        {
+            if (!this.m_IsInputActive || this.m_IsInputPaused) return;
+        }
+
+        private void OnStartHeavyAlternative(InputValue inputValue)
+        {
+            if (!this.m_IsInputActive || this.m_IsInputPaused) return;
+        }
+
+        private void OnStartBlock(InputValue inputValue)
+        {
+            if (!this.m_IsInputActive || this.m_IsInputPaused) return;
+        }
+
+        private void OnEndBlock(InputValue inputValue)
+        {
+            if (!this.m_IsInputActive || this.m_IsInputPaused) return;
+        }
+        
+        // -----------------------------------------------------
+        // Special-Action related Events
+        // -----------------------------------------------------
+
+        private void OnDodge(InputValue inputValue)
+        {
+            if (!this.m_IsInputActive || this.m_IsInputPaused) return;
+        }
+        
+        // -----------------------------------------------------
+        // Menu related Events
+        // -----------------------------------------------------
+
+        private void OnPause(InputValue inputValue)
+        {
+            throw new NotImplementedException();
         }
         
         #endregion Event Handlers
-        
+
+        #region - - - - - - Methods - - - - - -
+
+        public void Pause()
+            => this.m_IsInputActive = true;
+
+        public void OnUnpaused()
+            => this.m_IsInputActive = false;
+
+        #endregion Methods
+
     }
     
 }

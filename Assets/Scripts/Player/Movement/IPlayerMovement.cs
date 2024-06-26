62ee43e338065f907dd807c8141a5cf226ac258b
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-06-27 02:07:58 +1000

    Create PlayerMovement component

---
+++ Assets/Scripts/Player/Movement/IPlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace ThatOneSamuraiGame.Scripts.Player.Movement
+{
+    
+    public interface IPlayerMovement
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        void MovePlayer(Vector2 moveDirection);
+
+        void SprintPlayer(bool isSprinting);
+
+        #endregion Methods
+
+    }
+    
+}
---
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace ThatOneSamuraiGame.Scripts.Player.Movement
+{
+    
+    public class PlayerMovement : MonoBehaviour, IPlayerMovement
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        public void MovePlayer(Vector2 moveDirection)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SprintPlayer(bool isSprinting)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion Methods
+
+    }
+    
+}

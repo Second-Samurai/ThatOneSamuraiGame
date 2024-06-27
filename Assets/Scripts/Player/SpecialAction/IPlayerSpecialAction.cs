e7bd3eb9f1faaec5b38420d260160ecb061f2c0f
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-06-27 23:52:05 +1000

    Add SpecialAction for player input

---
+++ Assets/Scripts/Player/SpecialAction/IPlayerSpecialAction.cs
@@ -0,0 +1,15 @@
+namespace ThatOneSamuraiGame.Scripts.Player.SpecialAction
+{
+    
+    public interface IPlayerSpecialAction
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        void Dodge();
+
+        #endregion Methods
+
+    }
+    
+}
---
+++ Assets/Scripts/Player/SpecialAction/PlayerSpecialAction.cs
@@ -0,0 +1,22 @@
+using System;
+using ThatOneSamuraiGame.Scripts.Player.Attack;
+using UnityEngine;
+
+namespace ThatOneSamuraiGame.Scripts.Player.SpecialAction
+{
+    
+    public class PlayerSpecialAction : MonoBehaviour, IPlayerSpecialAction
+    {
+        
+        #region - - - - - - Methods - - - - - -
+
+        void IPlayerSpecialAction.Dodge()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion Methods
+        
+    }
+    
+}

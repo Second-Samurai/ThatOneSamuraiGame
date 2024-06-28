1c72f9c8d3d044a87d848753ba89df1524f1a548
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-06-29 03:26:58 +1000

    Fix implementation naming

--- Assets/Scripts/Player/Attack/PlayerAttackHandler.cs
+++ Assets/Scripts/Player/Attack/PlayerAttackHandler.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 namespace ThatOneSamuraiGame.Scripts.Player.Attack
 {
     
-    public class PlayerAttackHandler : MonoBehaviour, IPlayerAttackHandler
+    public class PlayerAttackHandler : MonoBehaviour, IPlayerAttackHandler, IPlayerAttackState
     {
+
+        #region - - - - - - Fields - - - - - -
+
+        private bool m_IsWeaponSheathed;
+
+        #endregion Fields
+        
+        #region - - - - - - Properties - - - - - -
+
+        bool IPlayerAttackState.IsWeaponSheathed
+            => this.m_IsWeaponSheathed;
+        
+        #endregion Properties
         
         #region - - - - - - Methods - - - - - -
 
@@ -24,17 +37,17 @@ namespace ThatOneSamuraiGame.Scripts.Player.Attack
             throw new NotImplementedException();
         }
 
-        void IPlayerAttackHandler.OnStartHeavyAlternative()
+        void IPlayerAttackHandler.StartHeavyAlternative()
         {
             throw new NotImplementedException();
         }
 
-        void IPlayerAttackHandler.OnStartBlock()
+        void IPlayerAttackHandler.StartBlock()
         {
             throw new NotImplementedException();
         }
 
-        void IPlayerAttackHandler.OnEndBlock()
+        void IPlayerAttackHandler.EndBlock()
         {
             throw new NotImplementedException();
         }

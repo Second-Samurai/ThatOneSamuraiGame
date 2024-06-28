390db8764415b594329980a1f6584f5be58c04be
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-06-29 03:25:59 +1000

    Add controlled camera state contract

---
+++ Assets/Scripts/Camera/IControlledCameraState.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace UnityTemplateProjects
+{
+    
+    public interface IControlledCameraState
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        Vector3 CurrentEulerAngles { get; }
+        
+        bool IsCameraViewTargetLocked { get; }
+
+        #endregion Properties
+        
+    }
+    
+}

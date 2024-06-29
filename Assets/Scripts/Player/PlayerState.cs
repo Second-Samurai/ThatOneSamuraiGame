abf9a4f37235c2f5a177f630c8937bedb3a29e01
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-06-30 03:44:04 +1000

    Add the PlayerState to hold a reference to the attack target

--- Assets/Scripts/Player/PlayerState.cs
+++ Assets/Scripts/Player/PlayerState.cs
@@ -3,12 +3,22 @@
 namespace ThatOneSamuraiGame.Scripts.Player
 {
     
-    public class PlayerState
+    public class PlayerState : MonoBehaviour
     {
 
+        #region - - - - - - Fields - - - - - -
+
+        [SerializeField]
+        private GameObject m_AttackTarget;
+
+        #endregion Fields
+        
         #region - - - - - - Properties - - - - - -
 
-        public GameObject AttackTarket { get; set; }
+        public GameObject AttackTarket
+        {
+            get { return this.m_AttackTarget; }
+        }
 
         #endregion Properties
         

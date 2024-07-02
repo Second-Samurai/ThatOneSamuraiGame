ae52a88f497bb6661d2fac163e8f3e96ebe1c12d
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-07-03 08:38:05 +1000

    Implement fields and services for PlayerSpecialActions

--- Assets/Scripts/Player/SpecialAction/PlayerSpecialAction.cs
+++ Assets/Scripts/Player/SpecialAction/PlayerSpecialAction.cs
@@ -1,5 +1,7 @@
 using System;
+using ThatOneSamuraiGame.Scripts.Camera;
 using ThatOneSamuraiGame.Scripts.Player.Attack;
+using ThatOneSamuraiGame.Scripts.Player.Movement;
 using UnityEngine;
 
 namespace ThatOneSamuraiGame.Scripts.Player.SpecialAction
@@ -7,12 +9,51 @@ namespace ThatOneSamuraiGame.Scripts.Player.SpecialAction
     
     public class PlayerSpecialAction : MonoBehaviour, IPlayerSpecialAction
     {
+
+        #region - - - - - - Fields - - - - - -
+
+        private ICameraController m_CameraController;
+        private ICombatController m_CombatController;
+        private IPlayerAttackState m_PlayerAttackState;
+        private IDamageable m_PlayerDamageHandler;
+        private IPlayerMovement m_PlayerMovement;
+        
+        private Animator m_Animator;
+        private bool IsDodging = false;
+        private PlayerFunctions m_PlayerFunctions;
+
+        #endregion Fields
+        
+        #region - - - - - - Lifecycle Methods - - - - - -
+
+        private void Start()
+        {
+            this.m_Animator = this.GetComponent<Animator>();
+            this.m_CameraController = this.GetComponent<ICameraController>();
+            this.m_CombatController = this.GetComponent<ICombatController>();
+            this.m_PlayerAttackState = this.GetComponent<IPlayerAttackState>();
+            this.m_PlayerDamageHandler = this.GetComponent<IDamageable>();
+            this.m_PlayerFunctions = this.GetComponent<PlayerFunctions>();
+            this.m_PlayerMovement = this.GetComponent<IPlayerMovement>();
+        }
+
+        #endregion Lifecycle Methods
         
         #region - - - - - - Methods - - - - - -
 
         void IPlayerSpecialAction.Dodge()
         {
-            throw new NotImplementedException();
+            
+        }
+
+        private void StartDodge()
+        {
+            
+        }
+
+        private void EndDodge()
+        {
+            
         }
 
         #endregion Methods

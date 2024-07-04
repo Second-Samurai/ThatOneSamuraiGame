946ef8e0b5425560ce1194557d013fd72a172cb4
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-07-05 01:21:25 +1000

    Implement interface for the PlayerState container

---
+++ Assets/Scripts/Player/Containers/IPlayerState.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace ThatOneSamuraiGame.Scripts.Player.Containers
+{
+    
+    public interface IPlayerState
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        GameObject AttackTarket { get; }
+        
+        bool CanOverrideMovement { get; set; }
+
+        #endregion Properties
+        
+    }
+    
+}
---
+++ Assets/Scripts/Player/Containers/PlayerState.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework.Constraints;
+using UnityEngine;
+
+namespace ThatOneSamuraiGame.Scripts.Player.Containers
+{
+    
+    public class PlayerState : MonoBehaviour, IPlayerState
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        [SerializeField]
+        private GameObject m_AttackTarget;
+
+        private bool m_CanOverrideMovement = false;
+
+        #endregion Fields
+        
+        #region - - - - - - Properties - - - - - -
+
+        GameObject IPlayerState.AttackTarket 
+            => this.m_AttackTarget;
+
+        bool IPlayerState.CanOverrideMovement
+        {
+            get => this.m_CanOverrideMovement;
+            set => this.m_CanOverrideMovement = value;
+        }
+
+        #endregion Properties
+        
+    }
+    
+}

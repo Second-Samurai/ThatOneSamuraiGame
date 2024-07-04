cfb91a91708b47c261dfdf3cce54d3e38557deee
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-07-05 01:21:03 +1000

    Create a seperate AnimationEventHandler for the Player

---
+++ Assets/Scripts/Player/Animation/PlayerAnimationEventHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using ThatOneSamuraiGame.Scripts.Base;
+using ThatOneSamuraiGame.Scripts.Player.Movement;
+
+namespace ThatOneSamuraiGame.Scripts.Player.Animation
+{
+    
+    /// <summary>
+    /// Responsible for handling animation events for the player.
+    /// </summary>
+    public class PlayerAnimationEventHandler : TOSGMonoBehaviourBase
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        private IPlayerMovement m_PlayerMovement;
+
+        #endregion Fields
+        
+        #region - - - - - - Lifecycle Methods - - - - - -
+
+        private void Start()
+        {
+            this.m_PlayerMovement = this.GetComponent<IPlayerMovement>();
+        }
+
+        #endregion Lifecycle Methods
+        
+        #region - - - - - - PlayerMovement Animation Events  - - - - - -
+        
+        // TODO: This animation event needs to be renamed to DisablePlayerMovement
+        public void OverrideMovement()
+        {
+            this.m_PlayerMovement.DisableMovement();
+        }
+
+        // TODO: This animation event needs to be renamed to EnablePlayerMovement
+        public void RemoveOverride()
+        {
+            this.m_PlayerMovement.EnableMovement();
+        }
+
+        #endregion PlayerMovement Animation Events
+        
+    }
+    
+}

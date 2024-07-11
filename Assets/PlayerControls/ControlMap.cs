4f10fae2c22838ae45f813fe6f0b43f2797a5e52
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-07-12 01:29:34 +1000

    Update scene setup for Mainscene to have first phase input fixes

--- Assets/PlayerControls/ControlMap.cs
+++ Assets/PlayerControls/ControlMap.cs
@@ -612,7 +612,7 @@ public partial class @ControlMap: IInputActionCollection2, IDisposable
                     ""name"": """",
                     ""id"": ""17ad6dd6-9a2a-4a9e-9dfa-bf9f27fa785b"",
                     ""path"": ""<Mouse>/leftButton"",
-                    ""interactions"": """",
+                    ""interactions"": ""Hold"",
                     ""processors"": """",
                     ""groups"": ""Keyboard"",
                     ""action"": ""StartHeavy"",

887978b6b11800effefecf9b45b3a71d86136a49
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-07-21 23:02:53 +1000

    Add SceneState DataContainer

---
+++ Assets/Scripts/Scene/DataContainers/SceneState.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace ThatOneSamuraiGame.Scripts.Scene.DataContainers
+{
+
+    /// <summary>
+    /// SceneState keeps track of non-persistent references and values during gameplay.
+    /// </summary>
+    /// <remarks>
+    ///     The lifetime of the container is throughout an entire sequence of scenes of related gameplay.
+    ///     This is not tied to the lifetime of a single unity scene.
+    /// </remarks>
+    public class SceneState : MonoBehaviour
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        // Public Fields
+        public bool IsGamePaused;
+        
+        #endregion Fields
+
+    }
+
+}

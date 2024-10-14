d90f31ca1fe7fe3c8f097764d4cd1aa484ec0699
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-10-14 22:50:18 +1100

    Create support manager + Add custom action runtime handlers

---
+++ Assets/Scripts/Infrastructure/CustomRuntimeHandlers/RemoveObjectOnStartHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace ThatOneSamuraiGame.Scripts.Infrastructure.CustomRuntimeHandlers
+{
+
+    public class RemoveObjectOnStartHandler : MonoBehaviour
+    {
+        
+        #region - - - - - - Unity Lifecycle Methods - - - - - -
+
+        private void OnEnable()
+            => Object.Destroy(this.gameObject);
+
+        #endregion Unity Lifecycle Methods
+
+    }
+
+}

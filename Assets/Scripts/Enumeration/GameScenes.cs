6205e0c6ac4195874db85cfdbc6c6f99286d8a25
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-10-22 22:01:35 +1100

    Implement a SmartEnum with GameScene

---
+++ Assets/Scripts/Enumeration/GameScenes.cs
@@ -0,0 +1,32 @@
+﻿namespace ThatOneSamuraiGame.Scripts.Enumeration
+{
+
+    public class GameScenes : SmartEnum
+    {
+        
+        #region - - - - - - Fields - - - - - -
+
+        public static GameScenes PersistenceScene = new("PersistenceScene", 0);
+        public static GameScenes MainGameScene = new("MainGameScene", 3);
+        
+        #endregion Fields
+  
+        #region - - - - - - Constructors - - - - - -
+
+        private GameScenes(string name, int value) : base(name, value) { }
+
+        #endregion Constructors
+
+        #region - - - - - - Methods - - - - - -
+
+        public static implicit operator int(GameScenes gameScenes)
+            => gameScenes.GetValue();
+
+        public static implicit operator string(GameScenes gameScenes)
+            => gameScenes.ToString();
+
+        #endregion Methods
+        
+    }
+
+}

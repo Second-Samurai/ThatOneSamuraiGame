406359f2dc2ce714d7e616aa2b5bacfb1c7118dd
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-11-21 00:16:52 +1100

    Adding custom static GameLogger with logging pass-throughs

---
+++ Assets/Scripts/Utility/GameLogging/GameLogger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace ThatOneSamuraiGame.GameLogging
+{
+
+    public static class GameLogger
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        public static void Log(string message)
+        {
+            #if UNITY_EDITOR || DEVELOPMENT_BUILD
+                Debug.Log($"[LOG]: {message}");
+            #endif
+        }
+
+        public static void LogWarning(string message)
+        {
+            #if UNITY_EDITOR || DEVELOPMENT_BUILD
+                Debug.LogWarning($"[WARNING]: {message}");
+            #endif
+        }
+        
+        public static void LogError(string message)
+        {
+            #if UNITY_EDITOR || DEVELOPMENT_BUILD
+                Debug.LogError($"[ERROR]: {message}");
+            #endif
+        }
+
+        #endregion Methods
+  
+    }
+
+}

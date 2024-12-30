e248328f2bbb2fa6701fed6488228fc13fc4fbde
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-12-30 18:46:28 +1100

    Refactor non movement behaviour from PlayerFinisherMovement

--- Assets/Scripts/Player/Movement/States/PlayerFinishMovement.cs
+++ Assets/Scripts/Player/Movement/States/PlayerFinishMovement.cs
@@ -1,31 +1,29 @@
 ﻿using System;
-using Cinemachine;
 using Enemies;
 using ThatOneSamuraiGame.Scripts.Player.Attack;
 using ThatOneSamuraiGame.Scripts.Player.Containers;
 using ThatOneSamuraiGame.Scripts.Player.Movement;
 using UnityEngine;
-using UnityEngine.Playables;
 
 public class PlayerFinishMovement : BasePlayerMovementState
 {
     #region - - - - - - Fields - - - - - -
 
-    private SphereCollider m_CollisionDetector; // May not be used
+    // private SphereCollider m_CollisionDetector; // May not be used
     // public PlayableAsset[] finishingMoves;
     // public bool bIsFinishing = false; // Might be no longer relevant
 
     // private PlayableDirector _cutsceneDirector;
-    private PDamageController damageController;
+    // private PDamageController damageController;
     // private List<Transform> enemies; // There is no reason to have all the enemies
     // private List<AISystem> enemiesCache; // This seems unnecessary
     // private GameObject targetEnemy;
 
     private readonly ILockOnSystem m_LockOnSystem;
-    private readonly Transform m_PlayerTransform;
-    private readonly MonoBehaviour m_ReferenceMonoBehaviour; // Required for Coroutine
+    // private readonly Transform m_PlayerTransform;
+    // private readonly MonoBehaviour m_ReferenceMonoBehaviour; // Required for Coroutine
     
-    private bool m_HasPlayedFinisherMove;
+    // private bool m_HasPlayedFinisherMove;
     private GameObject m_TargetEnemy;
 
     #endregion Fields
@@ -38,15 +36,15 @@ public class PlayerFinishMovement : BasePlayerMovementState
         MonoBehaviour refMonoBehaviour)
         :base(playerAnimator, playerTransform, movementState)
     {
-        this.m_PlayerTransform = playerTransform ?? throw new ArgumentNullException(nameof(playerTransform));
-        this.m_ReferenceMonoBehaviour = refMonoBehaviour ?? throw new ArgumentNullException(nameof(refMonoBehaviour));
+        // this.m_PlayerTransform = playerTransform ?? throw new ArgumentNullException(nameof(playerTransform));
+        // this.m_ReferenceMonoBehaviour = refMonoBehaviour ?? throw new ArgumentNullException(nameof(refMonoBehaviour));
         
-        lockOnSystem.OnNewLockOnTarget.AddListener(this.SetFinishingTargetEnemy);
-        lockOnSystem.OnLockOnDisable.AddListener(this.ResetState);
+        // lockOnSystem.OnNewLockOnTarget.AddListener(this.SetFinishingTargetEnemy);
+        // lockOnSystem.OnLockOnDisable.AddListener(this.ResetState);
         
         // _cutsceneDirector = this.m_ReferenceMonoBehaviour.GetComponent<PlayableDirector>();
         // BindToTrack("Cinemachine Track", GameManager.instance.MainCamera.GetComponent<CinemachineBrain>());
-        damageController = GameManager.instance.PlayerController.gameObject.GetComponent<PDamageController>();
+        // damageController = GameManager.instance.PlayerController.gameObject.GetComponent<PDamageController>();
     }
   
     public override void CalculateMovement() { }
@@ -54,23 +52,24 @@ public class PlayerFinishMovement : BasePlayerMovementState
     public override void ApplyMovement()
     {
         // Note: The finisher movement is intended to only run once.
-        if (this.m_HasPlayedFinisherMove || !this.m_TargetEnemy) return;
+        // if (this.m_HasPlayedFinisherMove || !this.m_TargetEnemy) return;
 
-        Guarding _EnemyGuardSystem = this.m_TargetEnemy.GetComponent<AISystem>().eDamageController.enemyGuard;
-        Animator _EnemyAnimator = this.m_TargetEnemy.GetComponentInChildren<Animator>();
-        
-        _EnemyGuardSystem.bRunCooldownTimer = false;
-        _EnemyGuardSystem.bRunRecoveryTimer = false;
-        _EnemyGuardSystem.uiGuardMeter.HideFinisherKey();
-
-        this.m_CollisionDetector.enabled = false;
-        damageController.DisableDamage();
+        // This appears to be irrelevant to the movement.
+        // Guarding _EnemyGuardSystem = this.m_TargetEnemy.GetComponent<AISystem>().eDamageController.enemyGuard;
+        // Animator _EnemyAnimator = this.m_TargetEnemy.GetComponentInChildren<Animator>();
+        //
+        // _EnemyGuardSystem.bRunCooldownTimer = false;
+        // _EnemyGuardSystem.bRunRecoveryTimer = false;
+        // _EnemyGuardSystem.uiGuardMeter.HideFinisherKey();
+        //
+        // this.m_CollisionDetector.enabled = false;
+        // damageController.DisableDamage();
         
         // _cutsceneDirector.Play();
         
         // Note: This is only a temporary solution
-        PlayerAttackState _PlayerAttackState = this.m_PlayerTransform.parent.GetComponent<IPlayerState>().PlayerAttackState;
-        _PlayerAttackState.CanAttack = false;
+        // PlayerAttackState _PlayerAttackState = this.m_PlayerTransform.parent.GetComponent<IPlayerState>().PlayerAttackState;
+        // _PlayerAttackState.CanAttack = false;
     }
     
     public override PlayerMovementStates GetState()
@@ -125,30 +124,30 @@ public class PlayerFinishMovement : BasePlayerMovementState
     //
     // TODO: Remove this to exist instead from the Player's attack controller.
     // - This should be seperated from the attack and movement state
-    public void KillEnemy()
-    {
-        this.m_TargetEnemy.GetComponent<AISystem>().bFinish = true; // Belongs to enemy
-        this.m_TargetEnemy.GetComponent<AISystem>().OnEnemyDeath(); // Belongs to enemy
-        this.m_CollisionDetector.enabled = true;
-        
-        // Below can be removed.
-        //GameManager.instance.RewindManager.IncreaseRewindAmount();
-        // playerInputScript.bCanAttack = true;
-        // playerInputScript.EnableMovement();
-        // playerInputScript.bAlreadyAttacked = false;
-        // playerInputScript.ResetAttack();
-        
-        // Note: This is only a temporary solution
-        IPlayerMovement _PlayerMovement = this.m_PlayerTransform.parent.GetComponent<IPlayerMovement>();
-        _PlayerMovement.EnableMovement();
-        
-        damageController.EnableDamage();
-
-        IPlayerAttackHandler _PlayerAttackHandler = this.m_PlayerTransform.parent.GetComponent<IPlayerAttackHandler>();
-        _PlayerAttackHandler.ResetAttack();
-        
-        // bIsFinishing = false;
-    }
+    // public void KillEnemy()
+    // {
+    //     this.m_TargetEnemy.GetComponent<AISystem>().bFinish = true; // Belongs to enemy
+    //     this.m_TargetEnemy.GetComponent<AISystem>().OnEnemyDeath(); // Belongs to enemy
+    //     this.m_CollisionDetector.enabled = true;
+    //     
+    //     // Below can be removed.
+    //     //GameManager.instance.RewindManager.IncreaseRewindAmount();
+    //     // playerInputScript.bCanAttack = true;
+    //     // playerInputScript.EnableMovement();
+    //     // playerInputScript.bAlreadyAttacked = false;
+    //     // playerInputScript.ResetAttack();
+    //     
+    //     // Note: This is only a temporary solution
+    //     IPlayerMovement _PlayerMovement = this.m_PlayerTransform.parent.GetComponent<IPlayerMovement>();
+    //     _PlayerMovement.EnableMovement();
+    //     
+    //     damageController.EnableDamage();
+    //
+    //     IPlayerAttackHandler _PlayerAttackHandler = this.m_PlayerTransform.parent.GetComponent<IPlayerAttackHandler>();
+    //     _PlayerAttackHandler.ResetAttack();
+    //     
+    //     // bIsFinishing = false;
+    // }
     //
     // // TODO: This is behaviour specific to the CutSceneDirector and not the FinishMovementController
     // private void BindToTrack(string trackName, Object val)
@@ -164,18 +163,18 @@ public class PlayerFinishMovement : BasePlayerMovementState
     // }
 
     // Transform is passed instead of GameObject as LockOnSystem relies on transform over gameobject.
-    private void SetFinishingTargetEnemy(Transform targetEnemyTransform)
-    {
-        this.m_TargetEnemy = targetEnemyTransform.gameObject;
-        // BindToTrack("Animation Track (1)", this.m_TargetEnemy);
-    }
+    // private void SetFinishingTargetEnemy(Transform targetEnemyTransform)
+    // {
+    //     // this.m_TargetEnemy = targetEnemyTransform.gameObject;
+    //     // BindToTrack("Animation Track (1)", this.m_TargetEnemy);
+    // }
 
     // Note: If necessary, an end state method can be applied to the BasePlayerMovementState class
     //      to handle transitioning between movement states.
-    private void ResetState()
-    {
-        this.m_TargetEnemy = null;
-        this.m_HasPlayedFinisherMove = false;
-    }
+    // private void ResetState()
+    // {
+    //     // this.m_TargetEnemy = null;
+    //     // this.m_HasPlayedFinisherMove = false;
+    // }
     
 }
--- Assets/Scripts/Player/PlayerFinisherController/PlayerFinisherController.cs
+++ Assets/Scripts/Player/PlayerFinisherController/PlayerFinisherController.cs
@@ -1,7 +1,9 @@
 ﻿using Cinemachine;
+using Enemies;
 using ThatOneSamuraiGame.Scripts.Base;
 using ThatOneSamuraiGame.Scripts.Camera.CameraStateSystem;
 using ThatOneSamuraiGame.Scripts.Player.Attack;
+using ThatOneSamuraiGame.Scripts.Player.Containers;
 using ThatOneSamuraiGame.Scripts.Player.Movement;
 using UnityEngine;
 
@@ -11,6 +13,8 @@ public interface IFinisherController
     #region - - - - - - Methods - - - - - -
 
     void StartFinishingAction();
+
+    void SetFinishingTargetEnemy(Transform targetEnemyTransform);
 
     #endregion Methods
 
@@ -28,6 +32,7 @@ public class PlayerFinisherController : PausableMonoBehaviour, IFinisherControll
     [RequiredField] public PDamageController m_PlayerDamageController;
     [RequiredField] public PlayerAttackHandler m_PlayerAttackHandler;
     [RequiredField] public CameraController m_CameraController;
+    public GameObject m_PlayerObject;
 
     public GameObject PlayerDetector; // Note: Used for enemies for detecting the player.
 
@@ -35,7 +40,7 @@ public class PlayerFinisherController : PausableMonoBehaviour, IFinisherControll
 
     private void Start()
     {
-        this.m_LockOnSystem.OnNewLockOnTarget.AddListener(this.SetFinishingTargetEnemy);
+        this.m_LockOnSystem.OnNewLockOnTarget.AddListener(((IFinisherController)this).SetFinishingTargetEnemy);
         
         CinemachineBrain _MainCameraCinemachineBrain = GameManager.instance.MainCamera.GetComponent<CinemachineBrain>();
         this.m_CutSceneDirector.BindToTrack("Cinemachine Track", _MainCameraCinemachineBrain);
@@ -44,6 +49,21 @@ public class PlayerFinisherController : PausableMonoBehaviour, IFinisherControll
     void IFinisherController.StartFinishingAction()
     {
         this.PlayerDetector.SetActive(false);
+
+        if (this.m_TargetEnemy)
+        {
+            Guarding _EnemyGuardSystem = this.m_TargetEnemy.GetComponent<AISystem>().eDamageController.enemyGuard;
+            _EnemyGuardSystem.bRunCooldownTimer = false;
+            _EnemyGuardSystem.bRunRecoveryTimer = false;
+            _EnemyGuardSystem.uiGuardMeter.HideFinisherKey();
+
+            this.m_PlayerDamageController.DisableDamage();
+            
+            // Note: This is only a temporary solution
+            PlayerAttackState _PlayerAttackState = this.m_PlayerObject.transform.parent
+                .GetComponent<IPlayerState>().PlayerAttackState;
+            _PlayerAttackState.CanAttack = false;
+        }
         
         ((IPlayerMovement)this.m_PlayerMovement).SetState(PlayerMovementStates.Finisher);
         this.m_CameraController.SelectCamera(SceneCameras.FollowPlayer);
@@ -51,13 +71,13 @@ public class PlayerFinisherController : PausableMonoBehaviour, IFinisherControll
     }
     
     // Transform is passed instead of GameObject as LockOnSystem relies on transform over gameobject.
-    private void SetFinishingTargetEnemy(Transform targetEnemyTransform)
+    void IFinisherController.SetFinishingTargetEnemy(Transform targetEnemyTransform)
     {
         this.m_TargetEnemy = targetEnemyTransform.gameObject;
         this.m_CutSceneDirector.BindToTrack("Animation Track (1)", this.m_TargetEnemy);
     }
 
-    private void EndFinishingActionSequence()
+    public void EndFinishingActionSequence()
     {
         this.PlayerDetector.SetActive(true);
         

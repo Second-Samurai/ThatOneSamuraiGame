9443aeb77bbd8ee000b5bf49bc272ff28d293742
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-01-03 15:39:56 +1100

    Refactor CameraStateSystem interface to its own file

---
+++ Assets/Scripts/Camera/CameraStateSystem/ICameraStateSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using ThatOneSamuraiGame.Scripts.Camera.CameraStateSystem;
+
+public interface ICameraStateSystem
+{
+
+    #region - - - - - - Methods - - - - - -
+
+    void Initialize();
+    
+    void SetState(SceneCameras selectedCamera);
+
+    List<ICameraState> GetCameraStates();
+
+    #endregion Methods
+
+}

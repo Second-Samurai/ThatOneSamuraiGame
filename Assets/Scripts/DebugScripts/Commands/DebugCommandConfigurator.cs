7efa06a02889aba467dd993fbfbcd4bd8a0491d5
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-02-02 18:22:46 +1100

    Rewired the debug command to handle a configurator class

---
+++ Assets/Scripts/DebugScripts/Commands/DebugCommandConfigurator.cs
@@ -0,0 +1,27 @@
+﻿
+public class DebugCommandConfigurator
+{
+
+    #region - - - - - - Fields - - - - - -
+
+    private DebugManager m_DebugManager;
+
+    #endregion Fields
+
+    #region - - - - - - Constructors - - - - - -
+
+    public DebugCommandConfigurator() 
+        => this.m_DebugManager = DebugManager.Instance;
+
+    #endregion Constructors
+
+    #region - - - - - - Methods - - - - - -
+
+    public void ConfigureCommands()
+    {
+        
+    }
+
+    #endregion Methods
+  
+}

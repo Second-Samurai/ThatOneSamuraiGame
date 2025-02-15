136ef2c9292beae3e98b0f6061abec88d879f022
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-02-15 18:23:18 +1100

    Implement generic weapon pickup class

---
+++ Assets/Scripts/Enumeration/GameTag.cs
@@ -0,0 +1,33 @@
+﻿using ThatOneSamuraiGame.Scripts.Enumeration;
+
+namespace ThatOneSamuraiGame
+{
+
+    public class GameTag : SmartEnum
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        public static GameTag Player = new("Player", 0);
+
+        #endregion Fields
+
+        #region - - - - - - Constructors - - - - - -
+
+        public GameTag(string name, int value) : base(name, value) { }
+
+        #endregion Constructors
+
+        #region - - - - - - Methods - - - - - -
+
+        public static implicit operator int(GameTag gameTag)
+            => gameTag.GetValue();
+
+        public static implicit operator string(GameTag gameTag)
+            => gameTag.ToString();
+
+        #endregion Methods
+        
+    }
+
+}
---
+++ Assets/Scripts/Enumeration/WeaponPickupType.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using ThatOneSamuraiGame;
+using ThatOneSamuraiGame.Scripts.Enumeration;
+
+namespace Enumeration
+{
+
+    [Serializable]
+    public class WeaponPickupType : SmartEnum
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        public static WeaponPickupType Katana = new("Katana", 0, weaponSystem =>
+        {
+            weaponSystem.SetWeapon(GameManager.instance.gameSettings.katanaPrefab);
+            AudioManager.instance.LightSaber = false;
+        });
+        public static WeaponPickupType LaserSword = new("LaserSword", 1, weaponSystem =>
+        {
+            weaponSystem.SetWeapon(GameManager.instance.gameSettings.laserSword);
+            AudioManager.instance.LightSaber = true;
+        });
+        // public static WeaponPickupType MoeHammer = new("MoeHammer", 2, () => { }) 
+
+        #endregion Fields
+  
+        #region - - - - - - Constructors - - - - - -
+
+        public WeaponPickupType(string name, int value, Action<IWeaponSystem> action) : base(name, value) 
+            => this.Action = action;
+
+        #endregion Constructors
+  
+        #region - - - - - - Properties - - - - - -
+
+        public Action<IWeaponSystem> Action { get; private set; }
+
+        #endregion Properties
+  
+        #region - - - - - - Methods - - - - - -
+
+        public static WeaponPickupType FromEnum(WeaponType weaponType)
+        {
+            switch (weaponType)
+            {
+                case WeaponType.Katana : return Katana;
+                case WeaponType.Lightsaber : return LaserSword;
+                default : return Katana;
+            }
+        }
+
+        public static implicit operator int(WeaponPickupType weaponPickupType)
+            => weaponPickupType.GetValue();
+
+        public static implicit operator string(WeaponPickupType weaponPickupType)
+            => weaponPickupType.ToString();
+
+        #endregion Methods
+    }
+
+}

a519e79918ac2cd316bec0392d9ec5e82ff23670
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-02-15 15:57:16 +1100

    Create PlayerWeaponSystem based on PSwordManager

---
+++ Assets/Scripts/Player/WeaponSystems/IWeaponSystem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace ThatOneSamuraiGame
+{
+
+    public interface IWeaponSystem
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        IWeaponEffectHandler WeaponEffectHandler { get; }
+
+        #endregion Properties
+  
+        #region - - - - - - Methods - - - - - -
+
+        void SetWeapon(GameObject weaponPrefab);
+        
+        void RevealWeapon();
+        
+        void HideWeapon();
+
+        void StartWeaponEffect(float slashAngle);
+
+        bool IsWeaponEquipped();
+
+        #endregion Methods
+  
+    }
+
+}
---
+++ Assets/Scripts/Player/WeaponSystems/PlayerWeaponSystem.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+namespace ThatOneSamuraiGame
+{
+
+    public class PlayerWeaponSystem : MonoBehaviour, IWeaponSystem
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        [RequiredField]
+        [SerializeField] 
+        private Transform m_WeaponHolder;
+        
+        private GameObject m_EquippedWeapon;
+        private IWeaponEffectHandler m_WeaponEffectHandler;
+
+        #endregion Fields
+
+        #region - - - - - - Properties - - - - - -
+
+        public IWeaponEffectHandler WeaponEffectHandler
+            => this.m_WeaponEffectHandler;
+
+        #endregion Properties
+  
+        #region - - - - - - Methods - - - - - -
+
+        public void SetWeapon(GameObject weaponPrefab)
+        {
+            if (this.IsWeaponEquipped())
+                this.RemoveWeapon();
+
+            this.m_EquippedWeapon = Instantiate(weaponPrefab, this.m_WeaponHolder);
+            this.m_EquippedWeapon.GetComponent<IInitialize<WeaponEffectInitializerData>>()
+                .Initialize(new WeaponEffectInitializerData { ParentTransform = this.m_WeaponHolder });
+            this.m_WeaponEffectHandler = this.m_EquippedWeapon.GetComponent<IWeaponEffectHandler>();
+        }
+        
+        public void RevealWeapon() 
+            => this.m_EquippedWeapon.SetActive(true);
+
+        public void HideWeapon() 
+            => this.m_EquippedWeapon.SetActive(false);
+
+        public void StartWeaponEffect(float slashAngle) 
+            => this.m_WeaponEffectHandler.CreateSlashEffect(slashAngle);
+
+        public bool IsWeaponEquipped() 
+            => this.m_EquippedWeapon != null;
+
+        private void RemoveWeapon()
+        {
+            Destroy(this.m_EquippedWeapon);
+            this.m_EquippedWeapon = null;
+        }
+
+        #endregion Methods
+  
+    }
+
+}

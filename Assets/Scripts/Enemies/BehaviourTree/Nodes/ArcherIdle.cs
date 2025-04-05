8e5475a4f0d23ccd98547ef34cd8733d293aff9a
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-04-06 02:47:02 +1100

    Create idle behaviour tree node for archer

---
+++ Assets/Scripts/Enemies/BehaviourTree/Nodes/ArcherIdle.cs
@@ -0,0 +1,46 @@
+﻿using MBT;
+using UnityEngine;
+
+[MBTNode(name = "Tasks/Idle Archer")]
+public class ArcherIdle : Leaf
+{
+
+    #region - - - - - - Fields - - - - - -
+
+    [SerializeField] private IntReference m_RepetitionCount = new();
+    
+    private Animator m_ArcherAnimator;
+    private bool m_IsIdleClipComplete;
+    private int m_PlayCount;
+    private int m_SelectedClipIndex;
+
+    #endregion Fields
+  
+    #region - - - - - - Unity Methods - - - - - -
+
+    private void Start()
+    {
+        this.m_ArcherAnimator = this.GetComponentInParent<Animator>();
+        
+        ArcherAnimationReciever _AnimationEventReceiver = this.GetComponentInParent<ArcherAnimationReciever>();
+        _AnimationEventReceiver.OnIdleCompletion.AddListener(() => this.m_IsIdleClipComplete = true);
+    }
+
+    #endregion Unity Methods
+  
+    #region - - - - - - Methods - - - - - -
+
+    public override NodeResult Execute()
+    {
+        if (!this.m_IsIdleClipComplete) return NodeResult.success;
+        
+        if (this.m_PlayCount > this.m_RepetitionCount.Value)
+            this.m_SelectedClipIndex = Random.Range(0, 3);
+        
+        ArcherAnimationEvents.ArcherIdle.Run(animator: this.m_ArcherAnimator, intValue: this.m_SelectedClipIndex);
+        return NodeResult.success;
+    }
+
+    #endregion Methods
+  
+}

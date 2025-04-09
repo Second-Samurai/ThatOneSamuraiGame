f98e388143a6d790c8b24f04ce5e218e41f0478c
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-04-09 22:53:10 +1000

    Add IK Target assignment for enemies with Aim Targets for IK constraints

---
+++ Assets/Scripts/Enemies/Animations/InverseKinematics/IKTargetAssigner.cs
@@ -0,0 +1,32 @@
+﻿using ThatOneSamuraiGame.Scripts.Base;
+using ThatOneSamuraiGame.Scripts.Scene.SceneManager;
+using UnityEngine;
+
+public class IKTargetAssigner : PausableMonoBehaviour
+{
+
+    #region - - - - - - Fields - - - - - -
+
+    private Transform m_AimTransform;
+
+    #endregion Fields
+
+    #region - - - - - - Unity Methods - - - - - -
+
+    private void Start()
+    {
+        GameObject _Player = SceneManager.Instance.SceneState.ActivePlayer;
+        IKLookAtAimTargetProvider _IKTargetProvider = _Player.GetComponent<IKLookAtAimTargetProvider>();
+        this.m_AimTransform = _IKTargetProvider.GetAimTarget();
+    }
+
+    private void Update()
+    {
+        if (this.IsPaused) return;
+
+        this.transform.position = this.m_AimTransform.position;
+    }
+
+    #endregion Unity Methods
+  
+}

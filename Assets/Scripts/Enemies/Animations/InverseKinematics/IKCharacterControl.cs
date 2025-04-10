65155d2776727039de32b14b3e634252f8f24c10
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-04-10 22:02:07 +1000

    Create IK Character Control

---
+++ Assets/Scripts/Enemies/Animations/InverseKinematics/IKCharacterControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public interface IIKWeightControl
+{
+
+    #region - - - - - - Methods' - - - - - -
+
+    void AnimateMasterWeight(float delta);
+
+    #endregion Methods'
+
+}
+
+public class IKCharacterControl : MonoBehaviour, IIKWeightControl
+{
+
+    #region - - - - - - Fields - - - - - -
+
+    public List<Rig> m_RigLayers;
+    [Range(0f, 1f)]
+    public float m_MasterWeightControl;
+    public AnimationCurve m_InterpolationCurve;
+
+    #endregion Fields
+
+    #region - - - - - - Methods - - - - - -
+
+    public void AnimateMasterWeight(float delta)
+    {
+        foreach (Rig _Rig in this.m_RigLayers)
+        {
+            _Rig.weight = this.m_InterpolationCurve.Evaluate(delta);
+        }
+    }
+
+    #endregion Methods
+
+}

a07ab7d4a2953f01612d39ba36170d3726bff0a1
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-04-16 22:39:22 +1000

    Begin implementing transform interpolation

---
+++ Assets/Scripts/Enemies/Animations/InverseKinematics/TransformReferenceInterpolator.cs
@@ -0,0 +1,25 @@
+﻿using ThatOneSamuraiGame.Scripts.Base;
+using UnityEngine;
+
+/// <summary>
+/// Responsible for interpolating transform between two positions
+/// </summary>
+public class TransformReferenceInterpolator : PausableMonoBehaviour
+{
+
+    #region - - - - - - Fields - - - - - -
+
+    [SerializeField] private Transform m_AffectedTransform;
+    [SerializeField] private Transform m_FirstTargetSourceTransform;
+    [SerializeField] private Transform m_SecondTargetSourceTransform;
+
+    [SerializeField] private float m_TransitionSpeed;
+    [SerializeField] private AnimationCurve m_TransitionCurve;
+
+    #endregion Fields
+
+    #region - - - - - - Methods - - - - - -
+
+    #endregion Methods
+  
+}

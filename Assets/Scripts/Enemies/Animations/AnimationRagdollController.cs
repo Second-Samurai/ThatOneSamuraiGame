2a7edfcf64550efa7cb263318b89c350a486751a
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-04-20 21:07:42 +1000

    Create the AnimationRagdollControll

---
+++ Assets/Scripts/Enemies/Animations/AnimationRagdollController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AnimationRagdollController : MonoBehaviour
+{
+
+    #region - - - - - - Fields - - - - - -
+
+    private List<Rigidbody> m_RagdollRigidbodies;
+
+    #endregion Fields
+
+    #region - - - - - - Unity Methods - - - - - -
+
+    private void Start()
+    {
+        this.m_RagdollRigidbodies = this.GetComponentsInChildren<Rigidbody>().ToList();
+        this.SwitchToKinematic();
+    }
+
+    #endregion Unity Methods
+
+    #region - - - - - - Methods - - - - - -
+
+    private void SwitchToKinematic()
+    {
+        for (int i = 0; i < this.m_RagdollRigidbodies.Count; i++)
+            this.m_RagdollRigidbodies[i].isKinematic = true;
+    }
+
+    private void SwitchToRagdoll()
+    {
+        for (int i = 0; i < this.m_RagdollRigidbodies.Count; i++)
+            this.m_RagdollRigidbodies[i].isKinematic = false;
+    }
+
+    #endregion Methods
+  
+}

ec778f86860f0df7b9bfd57b5568ed9d75e3d9a8
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-04-21 17:13:27 +1000

    Add EventObserver for enemy states and disabling behaviour for dead enemies

--- Assets/Scripts/Enemies/BehaviourTree/Nodes/Archer_Nodes/ArcherDeath.cs
+++ Assets/Scripts/Enemies/BehaviourTree/Nodes/Archer_Nodes/ArcherDeath.cs
@@ -26,6 +26,7 @@ public class ArcherDeath : Leaf
 
     [Header("Required Dependencies")]
     [SerializeField, RequiredField] private Animator m_Animator;
+    [SerializeField, RequiredField] private EnemyStateObserver m_EventObserver;
     [SerializeField, RequiredField] private AnimationRigControl m_RigControl;
     [SerializeField, RequiredField] private AnimationRagdollController m_RagdollController;
     [SerializeField, RequiredField] private Transform m_ArcherTransform;
@@ -48,6 +49,7 @@ public class ArcherDeath : Leaf
         
         GameValidator.NotNull(this.m_Animator, nameof(m_Animator));
         GameValidator.NotNull(this.m_ArcherTransform, nameof(m_ArcherTransform));
+        GameValidator.NotNull(this.m_EventObserver, nameof(m_EventObserver));
         GameValidator.NotNull(this.m_LockOnObserver, nameof(m_LockOnObserver));
         GameValidator.NotNull(this.m_RagdollController, nameof(m_RagdollController));
         GameValidator.NotNull(this.m_PlayerTransform, nameof(m_PlayerTransform));
@@ -72,6 +74,7 @@ public class ArcherDeath : Leaf
         this.m_WeaponBody.ChangeToDynamic();
         this.m_WeaponBody.ApplyForce(_Direction, 8); // Arbitrary Force
 
+        this.m_EventObserver.OnEnemyDeath.Invoke();
         this.StartCoroutine(this.KillArcher());
         this.m_HasPlayedAnimation = true;
         
---
+++ Assets/Scripts/Enemies/Common/BehaviourScriptDisabler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourScriptDisabler : MonoBehaviour
+{
+
+    #region - - - - - - Fields - - - - - -
+
+    [SerializeField] private List<MonoBehaviour> m_ScriptsToDisable;
+    [SerializeField] private List<GameObject> m_ObjectsToDisable;
+
+    #endregion Fields
+  
+    #region - - - - - - Methods - - - - - -
+
+    public void DisableObjectsAndScripts()
+    {
+        for (int i = 0; i < this.m_ScriptsToDisable.Count; i++)
+            this.m_ScriptsToDisable[i].enabled = false;
+        
+        for (int i = 0; i < this.m_ObjectsToDisable.Count; i++)
+            this.m_ObjectsToDisable[i].SetActive(false);
+    }
+
+    #endregion Methods
+  
+}

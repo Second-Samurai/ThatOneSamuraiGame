720f45311cf1054c359d2e1d023b92eb1bd3436d
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-04-29 20:40:22 +1000

    remove unecessary fields

--- Assets/Scripts/Enemies/BehaviourTree/Nodes/Archer_Nodes/AimToPlayer.cs
+++ Assets/Scripts/Enemies/BehaviourTree/Nodes/Archer_Nodes/AimToPlayer.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using MBT;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [AddComponentMenu("")]
 [MBTNode(name = "Tasks/Archer/Aim to Player")]
@@ -10,9 +11,10 @@ public class AimToPlayer : Leaf
 
     #region - - - - - - Fields - - - - - -
 
+    [FormerlySerializedAs("m_PlayerTransform")]
     [Header("Aiming Modifiers")]
-    [SerializeField] private TransformReference m_PlayerTransform = new();
-    [SerializeField] private FloatReference m_RotationSpeed = new(); // TODO: This needs to be utilized for finer control.
+    [SerializeField] private TransformReference m_PlayerTransformReference = new();
+    [SerializeField] private TransformReference m_ArcherTransformReference = new();
     [SerializeField] private FloatReference m_AimDetectionThreshold = new();
     [SerializeField] private float m_LayerWeightLerpTime;
     [SerializeField] private float m_TurnAngleSize;
@@ -24,7 +26,6 @@ public class AimToPlayer : Leaf
     // Required Dependencies
     private Animator m_ArcherAnimator;
     private AnimationRigControl m_RigControl;
-    private Transform m_ArcherTransform; // TODO: Chan this to instead retrieve from the behaviour tree variables
     
     // Turn motion fields
     private bool m_CanMakeTurnMotion = true;
@@ -46,7 +47,9 @@ public class AimToPlayer : Leaf
     {
         this.m_ArcherAnimator = this.GetComponentInParent<Animator>();
         this.m_RigControl = this.GetComponentInParent<AnimationRigControl>();
-        this.m_ArcherTransform = this.transform.root.transform;
+
+        GameValidator.NotNull(this.m_PlayerTransformReference, nameof(m_PlayerTransformReference));
+        GameValidator.NotNull(this.m_ArcherTransformReference, nameof(m_ArcherTransformReference));
 
         ArcherAnimationReciever _AnimationReceiver = this.GetComponentInParent<ArcherAnimationReciever>();
         _AnimationReceiver.OnTurnCompletion.AddListener(() => this.m_CanMakeTurnMotion = true);
@@ -74,14 +77,14 @@ public class AimToPlayer : Leaf
         }
 
         // Calculate target rotation
-        this.m_Forward = this.m_ArcherTransform.forward;
-        this.m_DirectionToTarget = (this.m_PlayerTransform.Value.position - this.m_ArcherTransform.position).normalized;
+        this.m_Forward = this.m_ArcherTransformReference.Value.forward;
+        this.m_DirectionToTarget = 
+            (this.m_PlayerTransformReference.Value.position - this.m_ArcherTransformReference.Value.position).normalized;
         this.m_DirectionToTarget.y = 0;
         Quaternion _TargetRotation = Quaternion.LookRotation(this.m_DirectionToTarget);
         
-        // Trigger rotation to player routine
-        // TODO: Move to its own method
-        this.m_AngleToPlayer = Quaternion.Angle(this.m_ArcherTransform.rotation, _TargetRotation);
+        // Trigger disable action
+        this.m_AngleToPlayer = Quaternion.Angle(this.m_ArcherTransformReference.Value.rotation, _TargetRotation);
         if (this.m_AngleToPlayer <= this.m_AimDetectionThreshold.Value)
         {
             this.m_HasDrawnBow = false;
@@ -91,22 +94,7 @@ public class AimToPlayer : Leaf
             return NodeResult.success;
         }
 
-        // TODO: Move to its own method
-        if (this.m_CanMakeTurnMotion)
-        {
-            // Trigger turn motion
-            float _SignedAngle = Vector3.SignedAngle(this.m_Forward, this.m_DirectionToTarget, Vector3.up);
-            if (_SignedAngle > 0f)
-            {
-                ArcherAnimationEvents.TurnRight.Run(this.m_ArcherAnimator);
-                this.StartCoroutine(this.TurnRotationOverride(this.m_TurnRightClipLength, _SignedAngle));
-            }
-            else if (_SignedAngle < 0f)
-            {
-                ArcherAnimationEvents.TurnLeft.Run(this.m_ArcherAnimator);
-                this.StartCoroutine(this.TurnRotationOverride(this.m_TurnLeftClipLength, _SignedAngle));
-            }
-        }
+        this.TriggerTurnMotion();
         
         return NodeResult.running;
     }
@@ -124,24 +112,45 @@ public class AimToPlayer : Leaf
             yield return null;
         }
     }
+
+    private void TriggerTurnMotion()
+    {
+        if (!this.m_CanMakeTurnMotion) return;
+        
+        float _SignedAngle = Vector3.SignedAngle(this.m_Forward, this.m_DirectionToTarget, Vector3.up);
+        if (_SignedAngle > 0f)
+        {
+            ArcherAnimationEvents.TurnRight.Run(this.m_ArcherAnimator);
+            this.StartCoroutine(this.TurnRotationOverride(this.m_TurnRightClipLength, _SignedAngle));
+        }
+        else if (_SignedAngle < 0f)
+        {
+            ArcherAnimationEvents.TurnLeft.Run(this.m_ArcherAnimator);
+            this.StartCoroutine(this.TurnRotationOverride(this.m_TurnLeftClipLength, _SignedAngle));
+        }
+    }
     
     // This is done since root motion could not be properly applied on anything but the base layer
     // Its highly possible that this is also a bug 'https://discussions.unity.com/t/root-motion-from-additive-layer-not-applied/845597'
     private IEnumerator TurnRotationOverride(float clipLength, float signedAngle)
     {
         float _Angle = signedAngle > 0f ? this.m_TurnAngleSize : -this.m_TurnAngleSize;
-        Quaternion _StartRotation = this.m_ArcherTransform.rotation;
+        Quaternion _StartRotation = this.m_ArcherTransformReference.Value.rotation;
         Quaternion _EndRotation = _StartRotation * Quaternion.Euler(0f, _Angle, 0f);
 
         float _ElapsedTime = 0f;
         while (_ElapsedTime < clipLength)
         {
-            this.m_ArcherTransform.rotation = Quaternion.Slerp(_StartRotation, _EndRotation, _ElapsedTime / clipLength);
+            this.m_ArcherTransformReference.Value.rotation = Quaternion.Slerp(
+                _StartRotation, 
+                _EndRotation, 
+                _ElapsedTime / clipLength);
+            
             _ElapsedTime += Time.deltaTime;
             yield return null;
         }
 
-        this.m_ArcherTransform.rotation = _EndRotation;
+        this.m_ArcherTransformReference.Value.rotation = _EndRotation;
         this.m_CanMakeTurnMotion = false;
     }
 
